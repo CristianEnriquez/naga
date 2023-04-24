52e257015813d9005c4932f6815aba84e1319190
cristianenriquez <enriquezer91@gmail.com>
2023-04-23 21:40:57 -0500

    llamada al metodo de persona duplicado

--- index.php
+++ index.php
@@ -6,4 +6,6 @@ require 'usuario.php';
 $usuario = new Usuario;
 $usuario->tipo = new Administrador;
 
-echo $usuario->tipo->saludo();
+echo $usuario->tipo->saludo(); 
+// probando rama nueva
+echo 'duplicado ' . $usuario->tipo->saludo(); 
